ad3c5740c2af640b7de119c3a073a8400d4bcab2
Tara Sherman <taradelin@gmail.com>
2020-09-11 21:15:17 -0400

    oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,3 +1,25 @@
+class Vehicle():
+    # base class
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
 # Write classes for the following class hierarchy:
 #
 #  [Vehicle]->[FlightVehicle]->[Starship]
